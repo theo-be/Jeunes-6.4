92f8a943bfcd12426a34b1e9aa367af1b7794ec8
Unvicss <106921987+Unvicss@users.noreply.github.com>
2023-06-10 01:21:56 +0200

    Add files via upload

---
+++ consultant.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>consultant</title>
+    <link href="consultant.css" rel="stylesheet">
+</head>
+<body>
+    <br><br>
+    
+    <div class="texteconsultant">
+    Validez cet engagement en prenant en compte sa valeur
+    </div>
+
+
+
+
+
+<div class="boxjeune" >
+
+
+<div class="jeune">
+    JEUNE
+</div>
+
+
+    <div class="texte">
+    NOM:<br>
+     PRENOM:<br>
+     DATE DE NAISSANCE:<br>
+     Mail:<br>
+     Réseau social:<br><br>
+     MON ENGAGEMENT:<br>
+     DUREE:<br>
+</div>
+
+
+<div class="petite-case">
+
+
+<div class="savoirs-jeune">
+    MES SAVOIRS ETRE
+</div>
+
+<div class="jesuis">
+    Je suis*
+</div>
+
+
+<div class="coche-jeune">
+        <input type="checkbox" checked disabled class="case-coche"><label>Autonome</label><br>
+        <input type="checkbox"  checked disabled class="case-coche"><label>Passionné</label><br>
+        <input type="checkbox" checked disabled class="case-coche"><label>Réfléchi</label><br>
+</div>
+</div>
+    </div>
+
+
+
+
+
+
+<div class="boxreferent" >
+
+
+<div class="referent">
+    REFERENT
+</div>
+
+
+    <div class="texte">
+    NOM:<br>
+     PRENOM:<br>
+     DATE DE NAISSANCE:<br>
+     Mail:<br>
+     Réseau social:<br><br>
+     PRESENTATION:<br>
+     DUREE:<br>
+</div>
+
+
+<div class="petite-case">
+
+
+<div class="savoirs-referent">
+    SES SAVOIRS ETRE
+</div>
+
+<div class="jeconfirme">
+    Je confirme sa(son)*
+</div>
+
+
+<div class="coche-referent">
+        <input type="checkbox" checked disabled class="case-coche"><label>Confiance</label><br>
+        <input type="checkbox"  checked disabled class="case-coche"><label>Bienveillance</label><br>
+        <input type="checkbox" checked disabled class="case-coche"><label>Respect</label><br>
+</div>
+</div>
+    </div>
+
+</body>
+</html>
