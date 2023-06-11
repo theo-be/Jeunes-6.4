c3bbbe93ae0348981088107ff776928fdadabbbd
theo-be <81959832+theo-be@users.noreply.github.com>
2023-06-11 22:54:29 +0200

    Update confirmationconsultant.php

--- php/confirmationconsultant.php
+++ php/confirmationconsultant.php
@@ -6,13 +6,7 @@ require_once "envoimail.php";
 if ($_SESSION["statut_client"] != "consultant")
     header("Location: /web/");
 
-
-
-
-
-
-
-
+// chargement de la base de donées et des tokens
 
 $contenufichier = file_get_contents("../data/bdd.json");
 $bdd = json_decode($contenufichier, false);
@@ -24,6 +18,7 @@ $token = json_decode($contenutoken, false);
 $indexref = array_search($_SESSION["idref"], $bdd->comptejeune[$_SESSION["idjeune"]]->idref);
 // echo $indexref;
 
+// on change les attributs du jeune
 $bdd->comptejeune[$_SESSION["idjeune"]]->statutdemandeconsultant = 2;
 $bdd->comptejeune[$_SESSION["idjeune"]]->messagestatutdemandeconsultant = "Demande acceptée par le consultant";
 $token->token[$_SESSION["tokenid"]]->etat = "complet";
@@ -32,7 +27,7 @@ $token->token[$_SESSION["tokenid"]]->etat = "complet";
 // envoyermail($bdd->comptejeune[$_SESSION["idjeune"]]->email, "jeunecon", "Votre demande de consultation", '');
 
 
-
+// sauvegarde
 
 $contenufichier = json_encode($bdd, JSON_PRETTY_PRINT);
 file_put_contents("../data/bdd.json", $contenufichier);
